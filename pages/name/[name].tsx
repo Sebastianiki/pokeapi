06c9dba1cd33aa8dcf11bb8fbeb1bdd3686b944b
Sebastian <reyesebastian1709@gmail.com>
2022-05-11 23:45:20 -0400

    add incremental static generation

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -116,7 +116,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokeNames.map(name => ({
       params: {name}
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -124,10 +124,15 @@ export const getStaticProps:GetStaticProps = async ({ params }) => {
 
   const { name } = params as { name:string }
 
+  const pokemon = await getPokemonInfo(name)
+
+  if (!pokemon) return { redirect: { destination: '/', permanent: false }}
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name)
-    }
+      pokemon
+    },
+    revalidate: 86400
   }
 }
 
